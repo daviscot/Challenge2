9beda5aa5cda554ced6ec53ce1ceb5339734ecf6
daviscot <36385536+daviscot@users.noreply.github.com>
2020-09-16 18:26:32 -0400

    Challenge2

--- chuck.py
+++ chuck.py
@@ -6,14 +6,11 @@ import requests
 url = 'https://api.chucknorris.io/jokes/random'
 
 def get_chuck_joke(api):
-    """
-    Get a random joke from the Chuck Norris API and return the serial JSON
-    :param api:
-    :return:serial JSON
-    """
     response = requests.get(api)
     return(json.loads(response.text))
 
 if __name__ == '__main__':
     joke = get_chuck_joke(url)
     print(joke['value'])
+
+#test 1234
